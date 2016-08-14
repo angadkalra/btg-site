ab29c22a6549f64e919a4eea15de82172ad2644e
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2016-08-14 13:47:07 -0700

    Connected controllers to views.

--- app/app.js
+++ app/app.js
@@ -1,1 +1,7 @@
 var app = angular.module('btg-site', []);
+app.controller('homeViewController', function() {});
+app.controller('aboutViewController', function() {});
+app.controller('getHelpViewController', function() {});
+app.controller('contactViewController', function() {});
+app.controller('emergencyViewController', function() {});
+app.controller('trainingViewController', function() {});
