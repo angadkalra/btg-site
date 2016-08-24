59943cc2ae14c36ca6edf4de2074993068edd3ad
alfaeddie <eduard-kagrimanyan@hotmail.com>
2016-08-23 20:15:30 -0700

    Updating routing to handle CORS and related

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 var fs = require('fs'),
 https = require('https'),
 express = require('express'),
+bodyParser = require('body-parser'),
+multer = require('multer'),
+upload = multer(),
 app = express();
 
 https.createServer(
@@ -8,7 +11,22 @@ https.createServer(
       cert: fs.readFileSync('./ssl/cert.pem') }
  ,app).listen(8080);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function (req, res) {
   res.header('Content-type', 'text/html');
-  return res.end('<h1>Welcome to the Secure BTG Portal.</h1>');
+  return res.end('<h1>Welcome to Secure BTG.</h1>');
+});
+
+app.post('/sendEmail', upload.array(), function(req, res, next){
+    console.log(req.body);
+    res.header('Content-type', 'text/html');
+    return res.end('<h1>You want some shit?.</h1>');
 });
