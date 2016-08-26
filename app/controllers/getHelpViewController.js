b61b4f132c5d2dc549f82232915c90637ab3e4d0
alfaeddie <eduard-kagrimanyan@hotmail.com>
2016-08-25 22:56:46 -0700

    Updating controller to toggle modal overlay when submitting email

--- app/controllers/getHelpViewController.js
+++ app/controllers/getHelpViewController.js
@@ -2,8 +2,17 @@ app.controller('getHelpViewController', ['$scope', function($scope) {
 	// This message controls what is displayed in getHelpView.html
 	// Changing this message here will change what is displayed in the ui-view
 	// consequently
+
 	$scope.message = "You are currently viewing the Get Help page!"
 	$scope.submit = function () {
+	    var toggleOverlay = function() {
+	        if ($('.modal-help-form.display-modal').length) {$('.modal-help-form').removeClass('display-modal');}
+	        else {$('.modal-help-form').addClass('display-modal');}
+	        if (!$('.modal-help-overlay').length) {$('.form-wrapper').append('<p class="modal-help-overlay">Sending...</p>');}
+	        else {$('.modal-help-overlay').remove();}
+	    }
+
+        toggleOverlay();
 	    $.ajax({
             url : 'https://localhost:8080/sendEmail',
             type : 'POST',
@@ -15,10 +24,12 @@ app.controller('getHelpViewController', ['$scope', function($scope) {
             crossDomain: true,
             success : function(data) {
                 console.log('Data: '+data);
+                toggleOverlay();
             },
             error : function(request,error)
             {
                 console.log(error);
+                toggleOverlay();
             }
         });
 	};
